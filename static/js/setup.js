93026dc5bd653c10ac9d4ae1e77dbd847b985781
James <you@example.com>
2021-09-10 15:26:39 +1200

    SmallChanges

---
+++ static/js/setup.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    /*
+        When the document has been loaded:
+            sets up teh listeners
+    */
+        "use strict"
+        setUpListeners();
+        setUpUI();
+    })
+
+    function setUpListeners() {
+        "use strict"
+        // adds the event listeners that are required
+        getElement("ButtonReset").addEventListener('click',function() {setUpUI();});
+        getElement("ButtonAdd").addEventListener('click',function() {addCityToList(getElement("selectTown").value);});
+        getElement("ButtonRemove").addEventListener('click',function() {removeCityFromList(getElement("selectedListItem"));});
+        getElement("buttonSave").addEventListener('click',function() {saveVisit();});
+        // Listener for an item being slected in teh list
+        getElement("travel_list").addEventListener('click',function(event) {selectListItem(event.target)});
+    }
+    
+    function setUpUI() {
+        // Clear teh input values
+        // Clear the visit information
+        getElement("selectTown").value = "07:00";
+        getElement("fuelPrice").value = null;
+
+        
+        // Clear teh product purchased list
+        getElement("travel_list").value = "";
+        writeToElement("travel_list","");
+    }
